7d8773613df0a6efbe60704747626c4ccdef60d2
404-5971 <141177946+404-5971@users.noreply.github.com>
2025-07-31 18:19:06 -0500

    fixed linting errors

--- index.tsx
+++ index.tsx
@@ -1,53 +1,47 @@
 /*
-* Vencord, a Discord client mod
-* Copyright (c) 2025 Vendicated and contributors*
-* SPDX-License-Identifier: GPL-3.0-or-later
-*/
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
 import definePlugin from "@utils/types";
 
-enum Mode
-{
+enum Mode {
     INSERT = "insert",
     NORMAL = "normal"
 }
 
 let mode: Mode = Mode.INSERT;
-function getScroller(): HTMLElement | null
-{
-  const wrapper = document.querySelector('[class*="messagesWrapper_"]');
-  if (!wrapper) return null;
+function getScroller(): HTMLElement | null {
+    const wrapper = document.querySelector('[class*="messagesWrapper_"]');
+    if (!wrapper) return null;
 
-  return wrapper.querySelector('[class*="scroller_"]') as HTMLElement;
+    return wrapper.querySelector('[class*="scroller_"]') as HTMLElement;
 }
 
-function handleKeyDown(key_event: KeyboardEvent): void
-{
-  const spaceKeyPressed: boolean = key_event.key === "Escape";
-  const hasInsertModeEnabled: boolean = mode == Mode.INSERT;
-  if ( hasInsertModeEnabled )
-  {
-    if ( spaceKeyPressed )
-    {
-      mode = Mode.NORMAL;
+function handleKeyDown(key_event: KeyboardEvent): void {
+    const spaceKeyPressed: boolean = key_event.key === "Escape";
+    const hasInsertModeEnabled: boolean = mode === Mode.INSERT;
+    if (hasInsertModeEnabled) {
+        if (spaceKeyPressed) {
+            mode = Mode.NORMAL;
+        }
+        return;
     }
-    return;
-  }
 
-  const scroller = getScroller();
-  key_event.preventDefault();
-  switch (key_event.key)
-  {
-    case "j":
-      scroller?.scrollBy({ top: 50, behavior: "instant" });
-      break;
-    case "k":
-      scroller?.scrollBy({ top: -50, behavior: "instant" });
-      break;
-    case "i":
-      mode = Mode.INSERT;
-      break;
-  }
+    const scroller = getScroller();
+    key_event.preventDefault();
+    switch (key_event.key) {
+        case "j":
+            scroller?.scrollBy({ top: 50, behavior: "instant" });
+            break;
+        case "k":
+            scroller?.scrollBy({ top: -50, behavior: "instant" });
+            break;
+        case "i":
+            mode = Mode.INSERT;
+            break;
+    }
 }
 
 
@@ -55,12 +49,10 @@ export default definePlugin({
     name: "vimMotion",
     description: "A plugin that allow you to use vim keymap to navigate in Discord",
     authors: [{ name: "yewspine", id: 1061439057722359808n }],
-    start()
-    {
-      window.addEventListener("keydown", handleKeyDown, true);
+    start() {
+        window.addEventListener("keydown", handleKeyDown, true);
     },
-    stop()
-    {
-      window.removeEventListener("keydown", handleKeyDown, true);
+    stop() {
+        window.removeEventListener("keydown", handleKeyDown, true);
     }
 });
